0392a139f141139467c2799b3b8dc4acd7258cdf
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2022-05-23 22:45:30 +0200

    add: responsive canvas resizing

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -7,15 +7,26 @@ const Canvas: FC<any> = props => {
     useEffect(() => {
         const canvas = canvasRef.current as HTMLCanvasElement
         const ctx = canvas.getContext('2d')
-
         // getContext can return null, so check for that case.
         if (!ctx) {
             throw new Error("getContext('2d') failed");
         }
+        // resize the canvas when window is resized
+        const resize = () => {
+            canvas.width = window.innerWidth - 4
+            canvas.height = window.innerHeight - 4
+        }
+        resize()
+        window.addEventListener('resize', resize)
 
+
+        // cleanup component by removing event listener when unmounted
+        return () => {
+            window.removeEventListener('resize', resize)
+        }
     }, [])
 
-    return <canvas ref={canvasRef} {...props}/>
+    return <canvas ref={canvasRef} {...props} />
 }
 
 export default Canvas
