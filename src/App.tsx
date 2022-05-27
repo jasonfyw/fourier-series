91bf4fc904fba2ff1afeb6f1a8630c2ec0b9938c
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2022-05-28 01:22:50 +0200

    add: drawing functionality

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
     return (
         <div className="App">
-            <Canvas />
+            <Canvas mode={'input'} />
         </div>
     );
 }
--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,17 +1,74 @@
-import React, { FC, useRef, useEffect } from 'react'
+import React, { FC, useRef, useEffect, useState } from 'react'
 
-const Canvas: FC<any> = props => {
+type CanvasProps = { mode: string }
+
+const Canvas: FC<CanvasProps> = props => {
+    const [mode, setMode] = useState(props.mode)
+    const [canvas, setCanvas] = useState<HTMLCanvasElement>(document.createElement('canvas'))
+    const [ctx, setCtx] = useState<CanvasRenderingContext2D>(document.createElement('canvas').getContext('2d')!)
+
+    const [drawing, setDrawing] = useState<boolean>(false)
+    const [points, setPoints] = useState<Array<[number,number]>>([])
 
     const canvasRef = useRef<HTMLCanvasElement>(null)
-
-    useEffect(() => {
-        const canvas = canvasRef.current as HTMLCanvasElement
-        const ctx = canvas.getContext('2d')
-        // getContext can return null, so check for that case.
-        if (!ctx) {
-            throw new Error("getContext('2d') failed");
+    
+    
+    /*
+    Event handlers for user input
+    */
+    const dragEventHandler = (e: MouseEvent) => {
+        if (drawing) {
+            setPoints(points => [...points, [e.pageX, e.pageY]])
         }
-        // resize the canvas when window is resized
+        redraw()
+    }
+    const mouseDownEventHandler = (e: MouseEvent) => {
+        setDrawing(true)
+        redraw()
+    }
+    const mouseUpEventHandler = (e: MouseEvent) => {
+        setDrawing(false)
+        redraw()
+    }
+    
+    const addEventHandlers = () => {
+        canvas.addEventListener('mousedown', mouseDownEventHandler);
+        canvas.addEventListener('mousemove', dragEventHandler);
+        canvas.addEventListener('mouseup', mouseUpEventHandler)
+    }
+    
+    const beginUserInput = () => {
+        ctx.lineCap = 'round'
+        ctx.lineJoin = 'round'
+        ctx.strokeStyle = 'black'
+        ctx.lineWidth = 1
+        
+        addEventHandlers()
+    }
+
+
+    /*
+    Drawing lines
+    */
+    const redraw = () => {
+        if (points.length > 0) {
+            // go to initial position
+            ctx.beginPath()
+            ctx.moveTo(points[0][0], points[0][1])
+            
+            for (const [x, y] of points) {
+                ctx.lineTo(x, y)
+                ctx.stroke()
+            }
+            ctx.closePath()
+        }
+    }
+    
+    // component did mount/unmount
+    useEffect(() => {
+        setCanvas(canvasRef.current as HTMLCanvasElement)
+        setCtx(canvas.getContext('2d') as CanvasRenderingContext2D)
+
         const resize = () => {
             canvas.width = window.innerWidth - 4
             canvas.height = window.innerHeight - 4
@@ -19,12 +76,21 @@ const Canvas: FC<any> = props => {
         resize()
         window.addEventListener('resize', resize)
 
+        beginUserInput()
 
         // cleanup component by removing event listener when unmounted
         return () => {
             window.removeEventListener('resize', resize)
+            canvas.removeEventListener('mousedown', mouseDownEventHandler);
+            canvas.removeEventListener('mousemove', dragEventHandler);
+            canvas.removeEventListener('mouseup', mouseUpEventHandler)
         }
-    }, [])
+    }, [canvas, drawing, points, canvas, ctx])
+
+    // TODO: change <mode> when updated
+
+
+  
 
     return <canvas ref={canvasRef} {...props} />
 }
