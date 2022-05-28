99a7c613a42ee761268f983938fbb27158ab408f
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2022-05-29 00:08:19 +0200

    add: integrated chakraUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Canvas from './components/Canvas';
 import './App.css';
+import { Box } from '@chakra-ui/react';
 
 function App() {
     return (
-        <div className="App">
+        <Box className="App" h={'100vh'} >
             <Canvas mode={'input'} />
-        </div>
+        </Box>
     );
 }
 
