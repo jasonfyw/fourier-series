014e6bf56a06651905bcb1d388dc2be204238057
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2022-05-30 02:12:43 +0200

    refactor: replace custom arange with lodash range

--- src/computations/integral.ts
+++ src/computations/integral.ts
@@ -1,21 +1,5 @@
 import { Complex, complex, multiply, add } from "mathjs"
-
-/**
- * Return an array of numbers from <start> to <stop> in increments of <step>
- * @param start starting index
- * @param stop ending index (not included)
- * @param step increment
- * @returns Array<number>
- */
-const arange = (start: number, stop: number, step: number): Array<number> => {
-    step = step || 1
-    let arr = []
-    for (let i = start; i < stop; i += step) {
-        arr.push(i)
-    }
-    return arr
-}
-
+import _ from "lodash"
 
 /**
  * Return the Riemann sum of a function f(x) with step size of <dx>. 
@@ -34,7 +18,7 @@ const integral = (
 ): Complex => {
     let sum = complex(0, 0)
 
-    for (const x of arange(a, b, dx)) {
+    for (const x of _.range(a, b, dx)) {
         sum = add(sum, multiply(f(x), dx)) as Complex
     }
 
