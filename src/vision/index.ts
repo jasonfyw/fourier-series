ff1407ec2249de4bcac0b07397e0c3ad04305399
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2022-11-07 19:22:07 -0500

    refactor: convert pixel array to binary array

--- src/vision/index.ts
+++ src/vision/index.ts
@@ -1,14 +1,14 @@
 import Image from 'image-js';
 const cannyEdgeDetector = require('canny-edge-detector');
 
-const getPixelArray = (img: Image) : Array<Array<number>> => {
+const getBinaryPixelArray = (img: Image) : Array<number> => {
     const grey = img.grey()
     const edge = cannyEdgeDetector.default(grey, { lowThreshold: 100, highThreshold: 10, gaussianBlur: 1. })
-    return edge.getPixelsArray()
+    return edge.getPixelsArray().map((pixel: Array<number>) => pixel[0] === 0 ? 0 : 1)
 }
 
 export const getImageEdgePath = (b64string: string) : Array<Array<number>> => {
-    let pixels = [[]] as Array<Array<number>>
+    let edgePixels = []
     let w = 0
     let h = 0
     let imageEdgePath = [[]] as Array<Array<number>>
@@ -16,7 +16,7 @@ export const getImageEdgePath = (b64string: string) : Array<Array<number>> => {
     Image.load(Buffer.from(b64string, 'base64')).then((img) => {
         w = img.width
         h = img.height
-        pixels = getPixelArray(img)
+        edgePixels = getBinaryPixelArray(img)
     })
 
     return imageEdgePath
